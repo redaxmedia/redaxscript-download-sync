c5524aafaf22244bb296d8814095badf02b452ac
redaxmedia <info@redaxmedia.com>
2018-12-16 14:51:24 +0100

    Remove page cache

--- tasks/ftpress.js
+++ tasks/ftpress.js
@@ -16,7 +16,7 @@ module.exports = () =>
 				command:
 				[
 					'mirror {SOURCE} {TARGET} --reverse --delete-first --parallel=10 --use-pget-n=10',
-					'rm -r www/htdocs/w00976cb/redaxscript/cache/pages',
+					'rm -rf www/htdocs/w00976cb/redaxscript/cache/pages',
 					'exit'
 				]
 			}
